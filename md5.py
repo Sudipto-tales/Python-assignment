a4970f308cef1aa986d7458871e9d8214e720e94
Sudipto <bcasudipta@gmail.com>
2024-12-17 23:42:56 +0530

    Update md5.py
    msg padding done

--- md5.py
+++ md5.py
@@ -1,8 +1,20 @@
-number = 2003
+#Padding: Append bits to the input to ensure its length is congruent to 448 modulo 512.
+def msg_padding(message):
+    message_bytes = bytearray(message, 'utf-8')
+    
+    org_len = len(message_bytes) * 8
+    message_bytes.append(0x80)
+    
+    while (len(message_bytes) * 8) % 512 != 448:
+        message_bytes.append(0)
+    
+    for i in range(8):
+        message_bytes.append(org_len >> (i * 8) & 0xFF)
+    
+    return message_bytes
 
-def padding(number):
- binary_representation = bin(number)[2:]
- return binary_representation.zfill(512)
+message = input("Enter Your Password: ")
+preprocessed_message = msg_padding(message)
 
-print(f"512-bit binary representation of {number} is:\n{padding(number)}")
-print(f"Length: {len(padding(number))} bits")
+print(f"Preprocessed message (hex): {preprocessed_message.hex()}")
+print(f"Length: {len(preprocessed_message) * 8} bits")
