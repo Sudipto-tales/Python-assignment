b37d5f21f74a328fcb2881fa8d8364c857d66769
Sudipto <bcasudipta@gmail.com>
2025-01-07 17:43:17 +0530

    Update bubble_sort.py

--- bubble_sort.py
+++ bubble_sort.py
@@ -1,8 +1,8 @@
 #Bubble Sort
 
 a = list(map(int, input("Enter array : ").split()))
-for pass_num in range(len(a) - 1):
-    for i in range(len(a) - 1 - pass_num):
+for n in range(len(a) - 1):
+    for i in range(len(a) - 1 - n):
         if a[i] > a[i + 1]:
             temp = a[i]
             a[i] = a[i + 1]
